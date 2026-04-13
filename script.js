dfd03e11ee2b9e0a816f8d355cd2d7177593d9e4
joaonzanatta-pixel <joao.nzanatta@gmail.com>
2026-04-13 10:41:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// doom elementy const body = document.querySelector('body');
+const btn = document.querySelector('.btn');
+const container = document.querySelector('.container');
+const input = document.querySelector('.input');
+const error = document.querySelector('.error');
+const result = document.querySelector('.result');
+
+form
